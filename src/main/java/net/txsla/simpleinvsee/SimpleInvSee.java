dc6101e04ec77f315bc679ae9641b302547dc4da
_Txsla <104021913+Tesla6426@users.noreply.github.com>
2024-12-15 12:11:26 -0800

    another plugin
    for nbt archives

---
+++ src/main/java/net/txsla/simpleinvsee/SimpleInvSee.java
@@ -0,0 +1,21 @@
+package net.txsla.simpleinvsee;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleInvSee extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // load commands
+
+        // not much else
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
